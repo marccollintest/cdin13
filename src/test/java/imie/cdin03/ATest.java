d1b555369bd7e209246db6f4582d952490f5e678
marccollin <marccollin@hotmail.fr>
2017-04-13 14:15:30 +0200

    First

---
+++ src/test/java/imie/cdin03/ATest.java
@@ -0,0 +1,14 @@
+package imie.cdin03;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ATest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
